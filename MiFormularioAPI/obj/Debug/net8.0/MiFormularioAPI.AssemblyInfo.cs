73163dc8cdc116917b513e4c68380e72fa1b530f
N4xtorq9 <n4xtor@gmail.com>
2025-02-27 11:42:15 -0500

    cambio

--- MiFormularioAPI/obj/Debug/net8.0/MiFormularioAPI.AssemblyInfo.cs
+++ MiFormularioAPI/obj/Debug/net8.0/MiFormularioAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiFormularioAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f011cce2ab00cf2338eb872e15b88dbfac2e197")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a279968c2b1274e91187bfd6362cfffb24942bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiFormularioAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiFormularioAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
